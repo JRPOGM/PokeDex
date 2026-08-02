ef86f3fc85e0ff66b84a02e649e9671540a748c5
JRPOGM <jrmacriss@gmail.com>
2026-08-02 12:34:36 -0700

    seeing if by multiple of 25 works

--- repl.go
+++ repl.go
@@ -224,7 +224,7 @@ func commandCatch(cfg *config, encounter ...string) error {
 	fmt.Println("Pokemon data has now been registered")
 	cfg.registeredPokemon[pokemon.Name] = pokemon
 	cfg.experienceBar += 30
-	if len(cfg.registeredPokemon) == 50 {
+	if len(cfg.registeredPokemon) % 25 == 0 {
 		fmt.Printf("Congratulations! You've managed to catch %v Pokemon! But there are many more still to catch. Don't stop traveling now!\n", len(cfg.registeredPokemon))
 	}
 	return nil
